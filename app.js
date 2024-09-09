426638e243659acbedae22d80fa286d6d08c83c8
vjdbarayuga <vjdbarayuga@gmail.com>
2024-09-09 23:31:12 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+// // RELATIONAL OPERATORS
+
+// let x = 5;
+// let y = 10;
+
+// //Greater Than
+// console.log(x > y); //FALSE
+
+// //Less Than
+// console.log(x < y); //TRUE
+
+// //Greater Than or Equal To
+// console.log(y >= x); // TRUE
+
+// //Less Than or Equal To
+// console.log(x <= y); //TRUE
+
+//EQUALITY
+// let a = "hello";
+// let b = "hello";
+// let c = 7;
+// let d = "7";
+
+// //Strict EQUALITY
+// console.log(a === b); //true (same value data type)
+// console.log(c === d); //false (different in data type)
+
+// //Strict Inequality
+// console.log(a !== b); //false (same value and data type)
+// console.log(c !== d); //true (different in data type)
+
+// //Loose Equality
+// console.log(c == d); //true (converted to a common data type)
+
+// //Loose Inequality
+// console.log(a != b); //false (converted to a common data type)
+
+//LOGICAL OPERATORS
+
+// let x = 10;
+// let y = 20;
+// let z = 30;
+
+// //AND
+
+// console.log(x < y && y < z); // (x is less than y and y is less than z)
+// console.log(x > y && y > z); // (both conditions are false)
+
+//OR
+
+// let a = 'hello';
+// let b = 'world';
+// let c = 'goodbye';
+
+// At lease one condition
+
+//console.log(a === b || a === c ); //(neither condition true)
+// console.log(a === b || b === c); // (one condition true)
+
+//NOT
+// let isSunny = true;
+// let isRainy = false;
+
+//INVERTING
+// console.log(!isSunny); // inverts it to true
+// console.log(!isRainy); // inverts it false
+
+//COMBINING WITH OTHER OPERATORS
+// console.log(!(isSunny && isRainy)); // (inverts false to true)
+//console.log(isSunny || !isRainy); //(one condition is true)
